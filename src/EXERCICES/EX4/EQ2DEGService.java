40ccfb85c9e3e2122ecd11642561027f15a5f998
Elhassak mohamed <mohamed.elhassak2@gmail.com>
2020-12-16 00:27:38 +0100

    add EX4 ResouderEq2Deg

--- src/EXERCICES/EX4/EQ2DEGService.java
+++ src/EXERCICES/EX4/EQ2DEGService.java
@@ -18,7 +18,7 @@ public class EQ2DEGService {
         if (a==0){
             respnse = "eq de 1er ordre...";
         }else{
-            double delta = -b*4*a*c;
+            double delta = b*b - 4*a*c;
             if(delta>0){
                 double x1=(-b-Math.sqrt(delta))/2*a;
                 double x2=(b+Math.sqrt(delta))/2*a;
